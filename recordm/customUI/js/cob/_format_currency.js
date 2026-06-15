81740b734afa6365cd1e2a5765df6078f3e7fddd
Miguel Mesquita <mm@cob.pt>
2026-06-15 17:00:40 +0100

    feat: adicionar suporte a $style[currency] em $references com evals

--- recordm/customUI/js/cob/_format_currency.js
+++ recordm/customUI/js/cob/_format_currency.js
@@ -1,10 +1,10 @@
 cob.custom.customize.push(function (core, utils, ui) {
     core.customizeAllColumns("*", (node, esDoc, colDef) => {
-        // Test $style[currency], by it self or with other styles 
+        // Test $style[currency], by it self or with other styles
         if(/\$style\[([^,]+,)*currency(,[^,]+)*\]/.exec(colDef.fieldDefDescription) != null) {
             let value = esDoc[colDef.field] ? esDoc[colDef.field][0] : null
             if(value) {
-                node.classList.add((value[0] === "-") ? "currency_negative" : "currency_positive")
+                node.classList.add((value[0] === "-" || value < 0) ? "currency_negative" : "currency_positive")
             }
         }
     })
